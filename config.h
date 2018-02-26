9d556864e6f21cd683d0df23beb6d6846b72df6f
SeongJae Park <sj38.park@gmail.com>
2018-02-26 22:30:08 +0100

    config: Add one more access pattern
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- config.h
+++ config.h
@@ -19,13 +19,19 @@ struct mregion mregions[] = {
 struct phase phases[] = {
 	{
 		.time_ms = 1000,
-		.nr_patterns = 1,
+		.nr_patterns = 2,
 		.patterns = (struct access[]) {
 			{
 				.mregion = &mregions[0],
 				.random_access = 1,
 				.stride = 64,
 			},
+			{
+				.mregion = &mregions[1],
+				.random_access = 0,
+				.stride = 4096,
+			},
+
 		},
 	},
 };
